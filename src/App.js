fea7ecd42809c148c3ca4f76d320a03fec7a8c66
dedane <evanskeema@gmail.com>
2022-10-27 13:52:34 +0300

    responsive-footer

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ function App() {
             
         <Divider mt={4} mb={2} />
         <Center mb={2} >
-          <Flex minWidth='max-content' alignItems='center' gap='20' >
+          <Flex minWidth='max-content' alignItems='center' gap='20' display={{ base: "none", md: "none", lg: "flex", xl: "flex" }}>
         <Box>
           <Image alt='zuri-team' src='https://ik.imagekit.io/ugyodiq15/Vector_8yGPghXEQ.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1666814065346' />
         </Box>
@@ -121,6 +121,22 @@ function App() {
         </Box>
       </Flex>
       </Center>
+
+      <Flex direction='column' alignItems='flex-end' gap='2' display={{ base: "flex", md: "flex", lg: "none", xl: "none" }}>
+        <Box>
+          <Image alt='zuri-team' src='https://ik.imagekit.io/ugyodiq15/Vector_8yGPghXEQ.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1666814065346' />
+        </Box>
+        <Spacer />
+          
+        <Box>
+          <Text>HNG Internship 9 Frontend Task</Text>
+        </Box>
+          <Spacer />
+          
+        <Box>
+          <Image alt='ingressive' src='https://ik.imagekit.io/ugyodiq15/ingressive_AI_l7vP0N.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1666814065785'></Image>
+        </Box>
+      </Flex>
       </Flex>
       
       
